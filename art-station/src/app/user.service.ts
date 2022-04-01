cd98c7de53372e5b46cac62dc937b49450a20de2
jtscottb <93688248+jtscottb@users.noreply.github.com>
2022-04-01 08:43:13 -0400

    created user service

---
+++ art-station/src/app/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  getUserArt() { }
+
+  getArt() { }
+}
