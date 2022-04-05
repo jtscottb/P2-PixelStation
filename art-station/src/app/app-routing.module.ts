12ed4d334ac3c2b0753ea2b15c573a530d7ddf27
jtscottb <93688248+jtscottb@users.noreply.github.com>
2022-04-05 09:28:48 -0400

    Login/Register button don't change colors

--- art-station/src/app/app-routing.module.ts
+++ art-station/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutComponent } from './about/about.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { PostComponent } from './post/post.component';
-import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AuthenticationGuard } from './route-guard/authentication.guard';
 import { AdminGuard } from './route-guard/admin.guard';
@@ -17,9 +13,6 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthenticationGuard] },
-  { path: 'about', component: AboutComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'admin', component: AdminDashboardComponent, canActivate: [AdminGuard] },
   { path: 'post', component: PostComponent },
   { path: 'user-profile', component: UserProfileComponent }
